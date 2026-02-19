22b1c4217c7e9e72bb788a6a3fe26fa2b27b3ec2
nutthunsk <nutthunsk@gmail.com>
2026-02-20 00:56:09 +0700

    j6

--- src/app.js
+++ src/app.js
@@ -28,6 +28,8 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use(express.static('public'))
+
 // static files
 app.use(express.static(path.join(__dirname, "../public")));
 
